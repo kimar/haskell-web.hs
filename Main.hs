f398588360ce0750048af5d41fe0fd65cf9290a7
Marcus Kida <marcus@kida.io>
2016-02-21 17:30:26 +1100

    Add request logger middleware

--- Main.hs
+++ Main.hs
@@ -2,9 +2,13 @@
 
 import Web.Scotty
 import Data.Text.Lazy.Encoding (decodeUtf8)
+import Network.Wai.Middleware.RequestLogger
 
 main :: IO ()
 main = scotty 3000 $ do
+  middleware logStdoutDev
+
+  -- take POST / and print body as text
   post "/" $ do
     jsonString <- decodeUtf8 <$> body
     text jsonString
